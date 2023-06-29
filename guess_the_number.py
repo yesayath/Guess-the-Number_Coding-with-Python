b6568b1852b9d5bcc7409d17f0a2a25261f200d9
yesayath <27696374+yesayath@users.noreply.github.com>
2023-06-29 11:41:50 +0300

    Changed print statements to use f-strings

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,7 +3,7 @@ number = random.randint(1, 10)
 
 player_name = input("Hello, what is your name? ")
 number_of_guesses = 0
-print('I\'m glad to meet you! {} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:'.format(player_name))
+print(f'I\'m glad to meet you! {player_name} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:')
 
 while number_of_guesses < 3:
     guess = int(input())
@@ -15,6 +15,6 @@ while number_of_guesses < 3:
     if guess == number:
         break
 if guess == number:
-    print( 'Congratulations {}, you guessed the number in {} tries!'.format(player_name, number_of_guesses))
+    print(f'Congratulations {player_name}, you guessed the number in {number_of_guesses} tries!')
 else:
-    print('Close but no cigar, you couldn\'t guess the number. \nWell, the number was {}.'.format(number))
+    print(f'Close but no cigar, you couldn\'t guess the number. \nWell, the number was {number}.')
